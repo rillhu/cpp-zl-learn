2daa329ee92b5b8e67bfb911dce08f3a1aa79651
hongfei <hongfei.hu@microej.com>
2022-06-06 13:27:26 +0800

    update 9-4

--- 9-4-array/array.h
+++ 9-4-array/array.h
@@ -5,4 +5,105 @@
 #ifndef INC_9_4_ARRAY_ARRAY_H
 #define INC_9_4_ARRAY_ARRAY_H
 
+#include <cassert>
+
+template<class T>
+class Array {
+private:
+    T *list;
+    int size;
+public:
+    Array(int sz = 50);         //constructor
+    Array(const Array<T> &a);   //copy constructor
+    ~Array();
+
+    Array<T> &operator=(const Array<T> &rhs);   //overload "="
+    T &operator[](int i);                       //overload "[]"
+    const T &operator[](int i) const;           //overload "[]" for const
+    operator T *();              //overload T* type convert
+    operator const T *() const;  //overload T* type convert for const
+
+    int getSize() const;
+
+    void reSize(int sz);
+};
+
+template<class T>
+Array<T>::Array(int sz) {
+    assert(sz >= 0);
+    size = sz;
+    list = new T(size);
+}
+
+
+template<class T>
+Array<T>::~Array() {
+    delete[]list;
+}
+
+template<class T>
+Array<T>::Array(const Array<T> &a) {
+    size = a.size;
+    list = new T[size];
+    for (int i = 0; i < size; i++) {
+        list[i] = a.list[i];
+    }
+}
+
+template<class T>
+Array<T> &Array<T>::operator=(const Array<T> &rhs) {
+    if (&rhs != this) {
+        if (size != rhs.size) {
+            delete[]list;
+            size = rhs.size;
+            list = new T[size];
+        }
+        for (int i = 0; i < size; ++i) {
+            list[i] = rhs.list[i];
+        }
+    }
+    return *this;
+}
+
+template<class T>
+T &Array<T>::operator[](int i) {
+    assert(i >= 0 && i < size);
+    return list[i];
+}
+
+template<class T>
+const T &Array<T>::operator[](int i) const {
+    assert(i >= 0 && i << size);
+    return list[i];
+}
+
+template<class T>
+Array<T>::operator T *() {
+    return list;
+}
+
+template<class T>
+Array<T>::operator const T *() const {
+    return list;
+}
+
+template<class T>
+int Array<T>::getSize() const {
+    return size;
+}
+
+template<class T>
+void Array<T>::reSize(int sz) {
+    assert(sz >= 0);
+    if (sz == size) return;
+    T *newList = new T[sz];
+    int n = (sz < size) ? sz : size;
+    for (int i = 0; i < n; ++i) {
+        newList[i] = list[i];
+    }
+    delete[] list;
+    list = newList;
+    size=sz;
+}
+
 #endif //INC_9_4_ARRAY_ARRAY_H
--- 9-4-array/main.cpp
+++ 9-4-array/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <iomanip>
+#include "array.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Array<int> a(10);
+    int count = 0;
+
+    int n;
+    cout << "Input a value >=2:";
+    cin >> n;
+
+    for (int i = 2; i < n; ++i) {
+        bool isPrime = true;
+        for (int j = 0; j < count; ++j) {
+            if(i%a[j]==0){
+                isPrime=false;
+                break;
+            }
+        }
+
+        if(isPrime){
+            if(count==a.getSize()){
+                a.reSize(count*2);
+            }
+            a[count++]=i;
+        }
+    }
+
+    for (int i = 0; i < count; ++i) {
+        cout<<setw(8)<<a[i];
+    }
+    cout<<endl;
+
     return 0;
 }
